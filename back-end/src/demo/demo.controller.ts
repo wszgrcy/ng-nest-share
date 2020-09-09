f4786e815a50d34d432fd9d05cf471a226ef0f24
wszgrcy <wszgrcy@gmail.com>
2020-09-09 21:55:50 +0800

    接口调通

--- back-end/src/demo/demo.controller.ts
+++ back-end/src/demo/demo.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-@Controller('demo')
+import { version } from '@share';
+@Controller('api')
 export class DemoController {
-    @Get()
-    getData(){
-    }
+  @Get('version')
+  getData() {
+    return { version };
+  }
 }
--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { version } from '@share';
 import { CyiaRepositoryService } from 'cyia-ngx-common/repository';
 import { DefaultEntity } from '../entity/default.entity';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,9 +10,12 @@ import { DefaultEntity } from '../entity/default.entity';
 })
 export class AppComponent {
   version = version;
-  constructor(private repository: CyiaRepositoryService) {
+  constructor(private repository: CyiaRepositoryService, private http: HttpClient) {
     this.repository.findOne(DefaultEntity).subscribe((res) => {
       console.log(res);
+    });
+    this.http.get('api/version').subscribe((e) => {
+      console.log(e);
     });
   }
 }
